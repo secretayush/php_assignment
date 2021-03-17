57cb23b5de152e242c97a01ceaab2b45871238ea
secretayush <iamayush0987@gmail.com>
2021-03-17 18:23:11 +0530

    TRN-32: Solved errors in image upload assignment

--- form2.php
+++ form2.php
@@ -11,52 +11,42 @@
 
       $f_name =  $_POST["fname"];
       $l_name =  $_POST["lname"];
-      $full_name = "Not a valid input";
-      $flag = 0;
 
       // cond to check valid string or not
-      if (($f_name == "") and ($l_name != "") and (ctype_alpha($l_name))) {
-              $flag = 1;
-              $full_name = $_POST['lname'];
+
+      if ( empty($f_name) || empty($l_name)) {
+
+        echo "Field is empty<br>";
+        include "php_assin2.html";
       }
-      elseif (($l_name == "") and ($f_name != "") and (ctype_alpha($f_name))) {
-            $flag = 1;
-            $full_name = $_POST['fname'];
-      }
-      elseif (($l_name != "") and ($f_name != "") and ((ctype_alpha($f_name)) and (ctype_alpha($l_name)))) {
-            $flag = 1;
-            $full_name = $_POST['fullname'];
-
-      }
-
-
-      // flag check cond
-      if ($flag === 1) {
-          $full_name = "Hello ".$full_name."<br>";
-      }
-
-      // assignment 2
-
-      $file = $_FILES['file']['name'];
-      // file's original path
-      $temp_name = $_FILES['file']['tmp_name'];
-
-
-      // move the uploaded file to the upload folder
-      $upload_file_check = move_uploaded_file($temp_name,$file);
-      $file_prop = explode('.',$file);
-
-      // cond to check if image is properly uploaded or not
-      if ($upload_file_check) {
-
-        echo "<br><img src=".$file." title=".$file_prop[0]." height='300px' width='200px' alt=".$file_prop[0]."><br>";
+      elseif ((ctype_alpha($f_name) == false) || (ctype_alpha($l_name) == false)) {
+        echo "Not a valid name";
+        include "php_assin2.html";
       }
       else{
-        echo "Image is not properly uploaded.<br>";
+
+        // assignment 2
+
+        $file = $_FILES['file']['name'];                  // file's original path
+        $temp_name = $_FILES['file']['tmp_name'];        // move the uploaded file to the upload folder
+        $upload_file_check = move_uploaded_file($temp_name,$file);
+        $file_prop = explode('.',$file);
+
+        // cond to check if image is properly uploaded or not
+        if ($upload_file_check) {
+
+          echo "<br><img src=".$file." title=".$file_prop[0]." height='300px' width='200px' alt=".$file_prop[0]."><br>";
+
+          //print full name under the image
+          echo $_POST['fullname'];
+        }
+        else{
+          echo "Image is not properly uploaded.<br>";
+          include "php_assin2.html";
+        }
+
+
       }
-
-    echo $full_name;
-
   }
   else{
      echo "Not a post method<br>";
