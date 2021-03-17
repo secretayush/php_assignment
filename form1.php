2983d3bdb62745b6cb762737db5a38d47c453426
secretayush <iamayush0987@gmail.com>
2021-03-17 17:55:03 +0530

    TRN-23: Solved issues of condition statement

--- form1.php
+++ form1.php
@@ -11,32 +11,25 @@
 
       $f_name =  $_POST["fname"];
       $l_name =  $_POST["lname"];
-      $full_name = "Not a valid input";
-      $flag = 0;
 
       // cond to check valid string or not
-      if (($f_name == "") and ($l_name != "") and (ctype_alpha($l_name))) {
-              $flag = 1;
-              $full_name = $_POST['lname'];
-      }
-      elseif (($l_name == "") and ($f_name != "") and (ctype_alpha($f_name))) {
-            $flag = 1;
-            $full_name = $_POST['fname'];
-      }
-      elseif (($l_name != "") and ($f_name != "") and ((ctype_alpha($f_name)) and (ctype_alpha($l_name)))) {
-            $flag = 1;
-            $full_name = $_POST['fullname'];
 
+      if ( empty($f_name) || empty($l_name)) {
+
+        echo "Field is empty<br>";
+        include "php_assin1.html";
       }
-
-
-      // flag check cond
-      if ($flag === 1) {
-          echo "Hello ".$full_name."<br>";
+      elseif ((ctype_alpha($f_name) == false) || (ctype_alpha($l_name) == false)) {
+        echo "Not a valid name";
+        include "php_assin1.html";
       }
       else{
-        echo $full_name;
+
+        $full_name = $_POST['fullname'];
+        echo "Hello ".$full_name;
       }
+
+
   }
   else{
      echo "Not a post method<br>";
