049de67c610c8fd4bbb5c78740d5a9b69db823e0
secretayush <iamayush0987@gmail.com>
2021-03-24 14:06:14 +0530

    TRN-68: Created new file form html and php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>API assignment</title>
+  <link rel="stylesheet" href="php_style1.css">
+</head>
+<body>
+  <?php include('Data.php');
+  ?>
+</body>
+</html>
