b176f586b0228453ae2f23e47552e9d534962bbc
secretayush <iamayush0987@gmail.com>
2021-03-24 14:51:42 +0530

    TRN-68: Added css file and proper comments

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
   <link rel="stylesheet" href="php_style1.css">
 </head>
 <body>
-  <?php include('Data.php');
+  <?php
+  include('Data.php');
+  //creating object of class
+  $response = new Data();
+  //calling the get api fun to get data from this api
+  $data = $response->get_api('https://www.innoraft.com/jsonapi/node/services');
+  //calling the get_data function to fetch the data and show
+  $response->get_data($data);
   ?>
 </body>
 </html>
