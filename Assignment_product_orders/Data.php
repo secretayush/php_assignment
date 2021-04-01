845236bc6919d65b972ef0bfd49dd68ab62ed5d6
secretayush <iamayush0987@gmail.com>
2021-04-01 18:58:05 +0530

    TRN-119:Completed Product order assignment

---
+++ Assignment_product_orders/Data.php
@@ -0,0 +1,11 @@
+<?php
+$data = array(
+  array('pd' => pd1, 'sp' => 5, 'sd' => '4thFeb', 'ct' => 'C1'),
+  array('pd' => pd1, 'sp' => 15, 'sd' => '5thFeb', 'ct' => 'C1'),
+  array('pd' => pd2, 'sp' => 50, 'sd' => '4thFeb', 'ct' => 'C1'),
+  array('pd' => pd3, 'sp' => 40, 'sd' => '6thFeb', 'ct' => 'C2'),
+  array('pd' => pd2, 'sp' => 75, 'sd' => '3rdFeb', 'ct' => 'C1'),
+  array('pd' => pd2, 'sp' => 65, 'sd' => '7thFeb', 'ct' => 'C1'),
+  array('pd' => pd4, 'sp' => 190, 'sd' => '8thFeb', 'ct' => 'C2')
+);
+?>
