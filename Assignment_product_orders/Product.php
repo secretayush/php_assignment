ccfe9ecbbef897c3abd2e78b41dd948b58de0af8
secretayush <iamayush0987@gmail.com>
2021-04-02 10:36:09 +0530

    TRN-119:Added usort function to sort the array

--- Assignment_product_orders/Product.php
+++ Assignment_product_orders/Product.php
@@ -10,21 +10,14 @@
       $this->data = $data;
     }
     //aranging the students
-    public function bubble_sort($arr) {
-      $size = count($arr)-1;
-      for ($i=0; $i<$size; $i++) {
-        for ($j=0; $j<$size-$i; $j++) {
-            $k = $j+1;
-            if ($arr[$k]['pd'] < $arr[$j]['pd']) {
-                // Swap elements at indices: $j, $k
-                list($arr[$j], $arr[$k]) = array($arr[$k], $arr[$j]);
-            }
-        }
-      }
-    return $arr;
+    public function arrsort($arr) {
+      usort($arr,function($a,$b){
+        return $a['pd'] <=> $b['pd'];
+      });
+      return $arr;
     }
     public function arrange(){
-      $data = $this->bubble_sort($this->data);
+      $data = $this->arrsort($this->data);
       for ($i=1; $i < count($data); $i++){
         $tot = 0;
         if ($data[$i]['pd'] == $data[$i-1]['pd']) {
