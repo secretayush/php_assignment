e06d618a6d4367137a018fe994c72027141512a3
secretayush <iamayush0987@gmail.com>
2021-04-07 17:35:23 +0530

    TRN-143: Added proper comments and solved indentation errors

--- Assignment_ipl_tournament/Data.php
+++ Assignment_ipl_tournament/Data.php
@@ -1,9 +1,10 @@
 <?php
   /**
-   * Class to sumbit data
+   * Class to sumbit data in database ipl
    */
   class Data
   {
+    // declared values in class varables
     public $venue;
     public $dom;
     public $team2;
@@ -15,55 +16,54 @@
     public $conn;
     function __construct()
     {
-      //connecting database to php code
+      //connecting database to php code defining constants
       define('DB_SERVER', 'localhost');
       define('DB_USERNAME', 'ayush');
       define('DB_PASSWORD', 'Ayush@123');
       define('DB_NAME', 'ipl');
+      //connecting data base to php
       $this->conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+      //condition to check if php is connected with database or not
       if (mysqli_connect_errno()) {
-        // connection faild
         echo "error in connection to database!!!";
       }
     }
-    // close connect to database
+    // destructor used to disconnect the connection from database
     function __destruct(){
+      // close connect to database
       $this->conn->close();
     }
+    //function to add team to team table
     public function add_team($t_name,$c_name)
     {
-      // $this->conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
-      if (mysqli_connect_errno()) {
-        echo "error in connection to database!!!";
+      //query to insert team name and captain name in team table
+      $team1 = "insert into teams(t_name,t_captain) values('$t_name','$c_name')";
+      //condition if team is added sucessfully
+      if ($this->conn->query($team1)) {
+        echo "Added Team";
       }
+      //condition if somthing is wrong
       else{
-         $team1 = "insert into teams(t_name,t_captain) values('$t_name','$c_name')";
-        if ($this->conn->query($team1)) {
-          echo "Added Team";
-        }
-        else{
-          echo "something worng";
-        }
+        echo "something worng";
       }
     }
+    //function to add venue in venues table in database
     public function add_venue($v_name)
     {
-      // $this->conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
-      if (mysqli_connect_errno()) {
-        echo "error in connection to database!!!";
+      //query to insert venue in venues table
+      $venue = "insert into venues(v_name) values('$v_name')";
+      //condition if query didnot run properly
+      if ($this->conn->query($venue)) {
+        echo "Added venue";
       }
       else{
-        $venue = "insert into venues(v_name) values('$v_name')";
-        if ($this->conn->query($venue)) {
-          echo "Added venue";
-        }
-        else{
-          echo "something worng";
-        }
+        echo "something worng";
       }
     }
+    //function to store data in match table
     public function store_data()
     {
+      //Stored values in class varables
       $this->venue = $_POST['venue'];
       $this->dom = $_POST['date_match'];
       $this->team1 = $_POST['team1'];
@@ -72,27 +72,31 @@
       $this->captain2 = $_POST['captain2'];
       $this->toss = $_POST['toss'];
       $this->match_win = $_POST['match_win'];
-
-      //venues
+      //query to take venues id from venue table where venue is matched with selected venue.
       $query_venue = "select v_id from venues where v_name = '$this->venue'";
+      //sending the query in mysql server and storing result
       $result_venue = $this->conn->query($query_venue);
+      //taking the data from the above query in associative array
       $row = mysqli_fetch_assoc($result_venue);
+      //id value is stored in varable to insert the value in match table
       $v_id = $row['v_id'];
-
-      //insert team 1 in teams table
+      //query to take team id from teams table where them name is matched with selected team name.
       $query_team = "select t_id from teams where t_name = '$this->team1'";
-      $result_team = $this->conn->query($query_team);;
+      $result_team = $this->conn->query($query_team);
+      //taking the data from the above query in associative array
       $row1 = mysqli_fetch_assoc($result_team);
+      //id value is stored in varable to insert the value in match table
       $t_id1 = $row1['t_id'];
-
-      //insert team2 data in teams table
+      //query to take team id from teams table where them name is matched with selected team name.
       $query_team = "select t_id from teams where t_name = '$this->team2'";
-      $result_team = $this->conn->query($query_team);;
+      $result_team = $this->conn->query($query_team);
+      //taking the data from the above query in associative array
       $row = mysqli_fetch_assoc($result_team);
+      //id value is stored in varable to insert the value in match table
       $t_id2 = $row['t_id'];
-
-      //insert match data in match table
+      //query to insert data in match table
       $match = "insert into matchs(match_date,v_id,result,team_1,team_2,toss) values('$this->dom','$v_id','$this->match_win','$t_id1','$t_id2','$this->toss')";
+      //condition to check is data is inserted properly or not
       if ($this->conn->query($match)) {
         echo "Added data in match table!!";
       }
@@ -101,5 +105,4 @@
       }
     }
   }
-
  ?>
--- Assignment_ipl_tournament/match.php
+++ Assignment_ipl_tournament/match.php
@@ -1,104 +1,128 @@
 <?php
+  //including data in the html to create object of class
   include('Data.php');
+  //created object of data class
   $obj = new Data();
-  if (mysqli_connect_errno()) {
-    echo "error in connection to database!!!";
-  }
+  //if button send is clicked then run below code
   if (isset($_POST['send'])){
     $obj->store_data();
   }
  ?>
-
 <!DOCTYPE html>
 <html>
 <head>
   <title>Ipl tournament</title>
 </head>
 <body>
-  <!-- form to post data in data base -->
+  <!-- form to post match data in data base -->
   <h1>Added match data </h1>
   <form action="" method="post">
+    <!-- venue input -->
     <label for="venue">Venue : </label>
     <select name="venue">
-      <!-- code to select the values from table and show in dropdown menu -->
+      <!-- selecting the values from table and show in dropdown menu -->
       <?php
-      $query2 = "select * from venues;";
-      $result2 = $obj->conn->query($query2);
-      while($rows1 = mysqli_fetch_assoc($result2)){
-        echo '<option value="'.$rows1['v_name'].'">'.$rows1['v_name'].'</option>';
-      }
+        //query to fetch all the columns from venues table
+        $query2 = "select * from venues;";
+        //sending the query in mysql server and storing result
+        $result2 = $obj->conn->query($query2);
+        //travasing through the selected value using associtive array
+        while($rows1 = mysqli_fetch_assoc($result2)){
+          //creating multiple options of drop down menu
+          echo '<option value="'.$rows1['v_name'].'">'.$rows1['v_name'].'</option>';
+        }
       ?>
-    </select><br> <br>
-
+    </select><br><br>
+    <!-- date of match input -->
     <label for="date_match">Date of match : </label>
     <input type="date" name="date_match"> <br><br>
-
+    <!-- Team 1 input from drop down options -->
     <label for="team1">Team 1 : </label>
+    <!-- selecting the values from table and show in dropdown menu -->
     <select name="team1">
       <?php
-      $query1 = "select t_name from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows2 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows2['t_name'].'">'.$rows2['t_name'].'</option>';
-      }
+        //query to fetch t_name from teams table
+        $query1 = "select t_name from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        while ($rows2 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows2['t_name'].'">'.$rows2['t_name'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- Team 2 input from drop down options -->
     <label for="team2">Team 2 : </label>
     <select name="team2">
+      <!-- selecting the values from table and show in dropdown menu -->
       <?php
-      $query1 = "select t_name from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows2 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows2['t_name'].'">'.$rows2['t_name'].'</option>';
-      }
+        //query to fetch t_name from teams table
+        $query1 = "select t_name from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        //travasing through the selected value using associtive array
+        while ($rows2 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows2['t_name'].'">'.$rows2['t_name'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- Captain 1 input from drop down options -->
     <label for="captain1">Captain team 1 :</label>
+    <!-- selecting captain from drop down table -->
     <select name="captain1">
       <?php
-      $query1 = "select t_captain from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows1 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows1['t_captain'].'">'.$rows1['t_captain'].'</option>';
-      }
+        //query to fetch t_captain from teams table
+        $query1 = "select t_captain from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        //travasing through the selected value using associtive array
+        while ($rows1 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows1['t_captain'].'">'.$rows1['t_captain'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- Captain 2 input from drop down options -->
     <label for="captain2">Captain team 2 : </label>
     <select name="captain2">
       <?php
-      $query1 = "select t_captain from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows1 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows1['t_captain'].'">'.$rows1['t_captain'].'</option>';
-      }
+        //query to select t_captain from teams table
+        $query1 = "select t_captain from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        //travasing through the selected value using associtive array
+        while ($rows1 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows1['t_captain'].'">'.$rows1['t_captain'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- Toss input from drop down options -->
     <label for="toss">Toss won by : </label>
     <select name="toss">
       <?php
-      $query1 = "select t_name from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows1 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows1['t_name'].'">'.$rows1['t_name'].'</option>';
-      }
+        //query to select t_namefrom teams table
+        $query1 = "select t_name from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        //travasing through the selected value using associtive array
+        while ($rows1 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows1['t_name'].'">'.$rows1['t_name'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- take input of winner from the match  -->
     <label for="match_win">Match won by : </label>
     <select name="match_win">
       <?php
-      $query1 = "select t_name from teams;";
-      $result1 = $obj->conn->query($query1);
-      while ($rows1 = mysqli_fetch_assoc($result1)){
-        echo '<option value="'.$rows1['t_name'].'">'.$rows1['t_name'].'</option>';
-      }
+        //query to select t_name from teams table
+        $query1 = "select t_name from teams;";
+        //sending the query in mysql server and storing result
+        $result1 = $obj->conn->query($query1);
+        //travasing through the selected value using associtive array
+        while ($rows1 = mysqli_fetch_assoc($result1)){
+          echo '<option value="'.$rows1['t_name'].'">'.$rows1['t_name'].'</option>';
+        }
       ?>
     </select><br><br>
-
+    <!-- Submit the data onclick -->
     <input type="submit" name="send" value="send"> <br>
   </form>
 </body>
